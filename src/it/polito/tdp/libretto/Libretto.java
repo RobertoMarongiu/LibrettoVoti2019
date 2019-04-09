b5499a2bbe4db55033ba7764a5de54431bdc1574
Roberto <Roberto@LAPTOP-12KMRIJF.homenet.telecomitalia.it>
2019-04-09 17:29:58 +0200

    Implementato punto 1

--- src/it/polito/tdp/libretto/Libretto.java
+++ src/it/polito/tdp/libretto/Libretto.java
@@ -1,5 +1,29 @@
 package it.polito.tdp.libretto;
 
-public class Libretto {
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Libretto {
+	
+	private List<Voto> voti ;
+
+	public Libretto() {
+		
+		this.voti = new ArrayList<Voto>(); //qua è vuota ma non nulla
+	}
+	
+	/*public void add(int voto,String corso, LocalDate data) {
+		
+	}
+	*/
+	/**
+	 * Aggiunge nuovo voto al libretto
+	 * @param v il (@link Voto) da aggiungere
+	 */
+	public void add(Voto v) {
+		//metodo che delega la funzione
+		//se si dovesse modifica la classe voto con questo metodo non devo modificare la classe libretto
+		voti.add(v);
+	}
 }
